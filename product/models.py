8bc81a1f2133fd93b8c803144708f6cfdae097f5
4nsepulveda <sepulveda.salazar78@gmail.com>
2024-09-07 12:37:31 -0500

    primer envio

--- product/models.py
+++ product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Recommendation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user.name} recommends {self.product.name} with score {self.score}",
