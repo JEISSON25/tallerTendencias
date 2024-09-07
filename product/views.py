afcb15e91bd4fb3ae99ca7cf28516dce9c2027ad
4nsepulveda <sepulveda.salazar78@gmail.com>
2024-09-07 13:10:49 -0500

    Producto enviado ANdres

--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Product, User, Recommendation
+from .serializers import ProductSerializer, UserSerializer, RecommendationSerializer # type: ignore
+from django.db.models import Avg
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+class TopProductsViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Recommendation.objects.values('producto_id').annotate(avg_score=Avg('puntuacion')).order_by('-avg_score')
+        return Response(queryset)
--- recomendation/models.py
+++ recomendation/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from product.models import Product
+from user.models import User
+
+class Recommendation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user.name} recommends {self.product.name} with score {self.score}",
+
