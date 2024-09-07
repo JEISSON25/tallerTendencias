a75af8545b8621de0592602181098a22a8052810
YulexaAragon <134795507+YulexaAragon@users.noreply.github.com>
2024-09-07 12:25:25 -0500

    Yulexa9

--- PlataformaCursos/app/Lesson/models.py
+++ PlataformaCursos/app/Lesson/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.db import models 
+from ..course.models import Course 
 
-# Create your models here.
+class Lesson(models.Model): 
+    class Meta:  
+        verbose_name = "Lesson"  
+        verbose_name_plural = "Lesson"  
+
+    nombre = models.CharField("Nombre", max_length=100) 
+    contenido = models.CharField("Contenido ", max_length=200) 
+
+    idCourse = models.ForeignKey (  
+    Course,  
+    verbose_name="Course",  
+    on_delete=models.CASCADE,  
+    null=True,  
+    blank=True  
+    )  
