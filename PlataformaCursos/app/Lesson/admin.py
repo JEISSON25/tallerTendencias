16e6c720e7146b7331370f229952e51714a8b0b5
YulexaAragon <134795507+YulexaAragon@users.noreply.github.com>
2024-09-07 13:38:43 -0500

    Final

--- PlataformaCursos/app/Lesson/admin.py
+++ PlataformaCursos/app/Lesson/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from ..lesson.models import *
 
-# Register your models here.
+admin.site.register(Lesson)
--- PlataformaCursos/app/api/routers.py
+++ PlataformaCursos/app/api/routers.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from ..user.views import *
 from ..course.views import *
-#from ..lesson.views import *
+from ..lesson.views import *
 from ..enrollment.views import *
 from ..quiz.views import *
 from ..question.views import *
@@ -11,7 +11,7 @@ router = DefaultRouter()
 
 router.register(r'user', UserViewset, basename='user')
 router.register(r'course', CourseViewset, basename='course')
-router.register(r'lesson', UserViewset, basename='lesson')
+router.register(r'lesson', LessonViewset, basename='lesson')
 router.register(r'enrollment', EnrollmentViewset, basename='enrollment')
 router.register(r'quiz', QuizViewset, basename='quiz')
 router.register(r'question', QuestionViewset, basename='question')
