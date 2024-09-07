0e44d17fdcb4db38173e603ecda70d9ce78e0a9a
Miguel <miguel.miranda@affinipay.com>
2024-09-07 13:24:06 -0500

    names for events and asistants

--- Grupo2/app/Reservation/serializers.py
+++ Grupo2/app/Reservation/serializers.py
@@ -2,6 +2,16 @@ from rest_framework import serializers
 from .models import *
 
 class ReservationSerializers(serializers.ModelSerializer):
+    event_name = serializers.SerializerMethodField()
+    attendee_name = serializers.SerializerMethodField()
+    
     class Meta:
         model = Reservation
-        fields = '__all__'
+        fields = ['id', 'num_entrys', 'event_name', 'attendee_name']
+
+    def get_event_name(self, obj):
+        return obj.idEvent.name
+
+    def get_attendee_name(self, obj):
+        return obj.idAttendee.nombre
+        
