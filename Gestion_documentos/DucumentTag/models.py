2f77e5ededa41e7c1bd6bc9460b511fe803acb4f
FelipeCardona194 <cardonafelipe293@gmail.com>
2024-09-07 11:56:13 -0500

    Modelss

--- Gestion_documentos/DucumentTag/models.py
+++ Gestion_documentos/DucumentTag/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class DocumentTag(models.Model):
+    documento = models.ForeignKey(Document, on_delete=models.CASCADE)
+    etiqueta = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.documento.nombre} - {self.etiqueta.nombre}"
