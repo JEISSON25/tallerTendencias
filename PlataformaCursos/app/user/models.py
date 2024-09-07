ecd47e9695a9de7721832ab175d363fe68ed77cf
PaulaAlvarezO05 <paula.alvarez@correo.tdea.edu.co>
2024-09-07 11:29:55 -0500

    Paula1

---
+++ PlataformaCursos/app/user/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    class Meta: 
+        verbose_name = "User" 
+        verbose_name_plural = "Users" 
+    
+    nombre = models.CharField("Nombre", max_length=100)
+    apellido = models.CharField("Apellido", max_length=100)
+    documento = models.CharField("Documento", max_length=100)
+    telefono = models.CharField("Teléfono", max_length=100)
+    Email = models.EmailField("Email", max_length=100)
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
