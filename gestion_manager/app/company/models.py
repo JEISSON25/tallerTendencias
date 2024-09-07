e63419ccfa2d243d0a2152f4898c4dbd24668275
Juan Esteban Duque <juanestd1013@hotmail.com>
2024-09-07 11:20:51 -0500

    commit

---
+++ gestion_manager/app/company/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Company (models.Model):
+    
+    name = models.CharField('Nombre',max_length=100)
+    
+    def  __str__(self):
+        return f'{self.name}'
+
+    
