7764d15ec44bf1e1c61f1e12beae77ed087a0c41
manuruizb <ruizbedoyamanuela@gmail.com>
2024-09-07 12:03:17 -0500

    add router of Reservation

--- Grupo2/Reservation/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class ReservationSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Reservation
-        fields = '__all__'
--- Grupo2/app/api/routers.py
+++ Grupo2/app/api/routers.py
@@ -1,12 +1,14 @@
 from rest_framework.routers import DefaultRouter
 from ..Event.views import *
 from ..Attendee.views import *
+from ..Reservation.views import *
 
 
 router = DefaultRouter()
 
 router.register(r'Event', EventViewSet, basename='Event')
 router.register(r'Attendee', AttendeeViewSet, basename='Attendee')
+router.register(r'Reservation', ReservationViewset, basename='Reservation')
 
 
 urlpatterns = router.urls
