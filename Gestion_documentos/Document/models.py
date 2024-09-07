54afff20bb8f639f961f441a745a596e40f388ee
FelipeCardona194 <cardonafelipe293@gmail.com>
2024-09-07 11:54:06 -0500

    Models

--- Gestion_documentos/Document/models.py
+++ Gestion_documentos/Document/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    archivo = models.FileField(upload_to='documents/')
+    fecha_carga = models.DateTimeField(auto_now_add=True)
+    propietario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
