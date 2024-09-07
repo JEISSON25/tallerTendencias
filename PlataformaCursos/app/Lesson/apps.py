c922efcccf145ece1d9f6b7e9daff81c26b3701b
YulexaAragon <134795507+YulexaAragon@users.noreply.github.com>
2024-09-07 12:20:30 -0500

    Yulexa8

--- PlataformaCursos/app/Lesson/apps.py
+++ PlataformaCursos/app/Lesson/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LessonConfig(AppConfig):
+class lessonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lesson'
+    name = 'app.lesson'
