ae3071e0d13fe106129c2601779aa9d0d3839b95
4nsepulveda <sepulveda.salazar78@gmail.com>
2024-09-07 13:17:41 -0500

    Enviado Andres

--- product/models.py
+++ product/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
 
-    def __str__(self):
-        return self.name
+    def _str_(self):
+        return self.nombre
 
